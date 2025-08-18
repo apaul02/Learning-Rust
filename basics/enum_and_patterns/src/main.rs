2a538a2a0cb48b1dd22f6ef8492a6089c0565a84
apaul02 <captainankan@gmail.com>
2025-08-19 00:52:17 +0530

    enums done

--- basics/enum_and_patterns/src/main.rs
+++ basics/enum_and_patterns/src/main.rs
@@ -1,25 +1,74 @@
 fn main() {
-    println!("Hello world");
-    let random_coin = Coin::Quater;
-    let value = value_in_cents(random_coin);
-    println!("{}", value);
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("the config is {max}");
+    }
+
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+   }
+}
+
+fn check(x: &u32) -> bool {
+    match x {
+        1 => true,
+        2 => false,
+        3 => true,
+        _ => false
+        
+    }
+}
+#[derive(Debug)]
+enum UsState {
+    Alaska,
+    Alabama,
+}
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+        }
+    }
+    
 }
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quater
+    Quarter(UsState)
 }
 
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky Penny");
-            1
-         },
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quater => 25,
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    if let Coin::Quarter(state) = coin {
+        if state.existed_in(1900) {
+            Some(format!("{state:?} is pretty old, for america"))
+        }else {
+            Some(format!("{state:?} is pretty new for america"))
+        }
+    }else {
+        None
+    }
+}
+
+fn describe_state_quarter2(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return None;
+    };
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old for america"))
+
+    }else {
+        Some(format!("{state:?} is pretty new for america"))
     }
 }
