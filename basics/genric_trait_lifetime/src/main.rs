72e90cb7c8b63791d12027ee9a53530d16eb004e
apaul02 <captainankan@gmail.com>
2025-09-07 00:39:30 +0530

    some2

--- basics/genric_trait_lifetime/src/main.rs
+++ basics/genric_trait_lifetime/src/main.rs
@@ -6,8 +6,19 @@ fn main() {
         result = longest(&s1.as_str(), &s2.as_str());
         println!("The result is {result}");
     }
+    let novel = String::from("Call me Ishmael. Some years ago....");
+    let first_sentence = novel.split(".").next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence
+    };
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
 }
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+
